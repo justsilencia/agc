6d930d7d9133cb643654dc84a2066438efa5a5e5
Jeff Wilson <jeff@ngdevlabs.com>
2026-07-27 22:39:37 -0400

    canonical

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://agcbjj.com',
+	site: 'https://www.agcbjj.com',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
